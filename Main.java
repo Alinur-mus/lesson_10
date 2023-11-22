44cdc65f2103a6afc55de74645249bfb4eb98b24
Alinur-mus <148569986+Alinur-mus@users.noreply.github.com>
2023-11-22 19:38:56 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,113 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+//        Task 1
+//        System.out.println("Salem Java");
+//        System.out.println("Salem Java");
+//        System.out.println("Salem Java");
+//        System.out.println("Salem Java");
+//        System.out.println("Salem Java");
+//
+//        System.out.println();
+//        System.out.println("While циклі");
+//        int i =0;
+//        }
+//        while(i<5){
+//            System.out.println("Salem Java");
+//            i++;
+//         Task 2
+//int i = 0;
+//while(i<5){
+//    System.out.println(i+" ");
+//    i++;
+//}
+// Task 3
+//        int j =10;
+//while(j>5){
+//    j++;
+//    System.out.println(j+"");
+//}
+//        task 4
+//        int j = 0;
+//while(true){
+//    j++;
+//    System.out.println(j+" ");
+//    if(j==7){
+//        break;
+//    }
+//}
+//task5
+//        int j = 0;
+//        while(j<8){
+//            j++;
+//            if(j==5){
+//                continue;
+//            }
+//            System.out.println(j+" ");
+//        }
+//Problem 1
+//        1
+//        int i=0;
+//        while(i<151){
+//            i++;
+//            if (i%10==0){
+//                System.out.println(i);
+//            }
+//        }
+//2
+//        int i=0;
+//        while(i<41){
+//            i++;
+//            if(i%2==0){
+//                System.out.println(i);
+//            }
+//        }
+//        3
+//        int i=5;
+//        while (i<91){
+//            i++;
+//            if(i%5==0){
+//                System.out.println(i);
+//            }
+//        }
+//        4
+//        int i=0;
+//        while(i<30){
+//            i++;
+//            if(i%3!=0){
+//                System.out.println(i);
+//            }
+//        }
+//        Problem 2
+//        1
+//        for (int i=10; i<151; i=i+10){
+//            System.out.println(i);
+//        }
+//        2
+//        for(int i=2;i<41;i=i+2){
+//            System.out.println(i);
+//        }
+//        3
+//        for (int i=0;i<90;i=i+5){
+//            System.out.println(i);
+//        }
+//        4
+//        for(int i=0;i<30;i++){
+//            if(i%3!=0){
+//                System.out.println(i);
+//            }
+//        }
+//        double i=1;
+//        double factorial=1;
+//        while(i<6){
+//            i++;
+//            if(i%2!=0){
+//                factorial=factorial*i;
+//            }
+//        }System.out.println(factorial);
+//        double factorial=1;
+//        for(double i=1;i<51;i=i+2){
+//          factorial=factorial*i;
+//        }System.out.println(factorial);
+    }
+}
